7fcb9456e8349da9575531aedaed1302c8bde251
djowel <djowel@gmail.com>
2020-07-01 14:25:58 +0800

    - Formatting changes indent=3 - Better distribution for putch-bend (from Tim Noonan)

--- source/nexus_midi/nexus_midi.ino
+++ source/nexus_midi/nexus_midi.ino
@@ -1,8 +1,8 @@
 /*=======================================================================================
-   Copyright (c) 2016 Cycfi Research
+    Copyright (c) 2016 Cycfi Research
 
-   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
-  =======================================================================================*/
+    Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+   =======================================================================================*/
 #include "midi.hpp"
 #include "util.hpp"
 #include "MspFlash.h"
@@ -95,56 +95,56 @@ midi::midi_stream midi_out;
 ///////////////////////////////////////////////////////////////////////////////
 struct flash
 {
-    typedef unsigned char byte;
+   typedef unsigned char byte;
 
-    flash(byte* segment_)
+   flash(byte* segment_)
       : _segment(segment_)
-    {}
+   {}
 
-    void erase()
-    {
+   void erase()
+   {
       Flash.erase(_segment);
-    }
+   }
 
-    bool empty() const
-    {
+   bool empty() const
+   {
       return (*_segment == 0xff);
-    }
+   }
 
-    byte read() const
-    {
+   byte read() const
+   {
       if (empty())
-        return 0xff;
+         return 0xff;
       if (byte* p = find_free())
-        return *(p - 1);
+         return *(p - 1);
       return _segment[63];
-    }
+   }
 
-    void write(byte val)
-    {
+   void write(byte val)
+   {
       byte* p = find_free();
       if (p == 0)
       {
-        erase();
-        Flash.write(_segment, &val, 1);
+         erase();
+         Flash.write(_segment, &val, 1);
       }
       else
       {
-        Flash.write(p, &val, 1);
+         Flash.write(p, &val, 1);
       }
-    }
+   }
 
-  private:
+   private:
 
-    byte* find_free() const
-    {
+   byte* find_free() const
+   {
       for (int i = 0; i != 64; ++i)
-        if (_segment[i] == 0xff)
-          return &_segment[i];
+         if (_segment[i] == 0xff)
+            return &_segment[i];
       return 0;
-    }
+   }
 
-    byte* _segment;
+   byte* _segment;
 };
 
 // We use SEGMENT_B and SEGMENT_C to store program change and bank select data
@@ -161,7 +161,7 @@ int32_t save_delay_start_time = -1;
 
 void reset_save_delay()
 {
-  save_delay_start_time = millis();
+   save_delay_start_time = millis();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -170,16 +170,16 @@ void reset_save_delay()
 #ifdef NEXUS_TEST
 struct note
 {
-  void operator()(bool sw)
-  {
-    int state = edge(sw);
-    if (state == 1)
-      midi_out << midi::note_on{0, 80, 127};
-    else if (state == -1)
-      midi_out << midi::note_off{0, 80, 127};
-  }
+   void operator()(bool sw)
+   {
+      int state = edge(sw);
+      if (state == 1)
+         midi_out << midi::note_on{0, 80, 127};
+      else if (state == -1)
+         midi_out << midi::note_off{0, 80, 127};
+   }
 
-  edge_detector<> edge;
+   edge_detector<> edge;
 };
 
 note _note;
@@ -191,22 +191,22 @@ note _note;
 template <midi::cc::controller ctrl>
 struct controller
 {
-  static midi::cc::controller const ctrl_lsb = midi::cc::controller(ctrl | 0x20);
+   static midi::cc::controller const ctrl_lsb = midi::cc::controller(ctrl | 0x20);
 
-  void operator()(uint32_t val_)
-  {
-    uint32_t val = lp(val_);
-    if (gt(val))
-    {
-      uint8_t const msb = val >> 3;
-      uint8_t const lsb = (val << 4) & 0x7F;
-      midi_out << midi::control_change{0, ctrl_lsb, lsb};
-      midi_out << midi::control_change{0, ctrl, msb};
-    }
-  }
+   void operator()(uint32_t val_)
+   {
+      uint32_t val = lp(val_);
+      if (gt(val))
+      {
+         uint8_t const msb = val >> 3;
+         uint8_t const lsb = (val << 4) & 0x7F;
+         midi_out << midi::control_change{0, ctrl_lsb, lsb};
+         midi_out << midi::control_change{0, ctrl, msb};
+      }
+   }
 
-  lowpass<64, int32_t> lp;
-  gate<noise_window, int32_t> gt;
+   lowpass<64, int32_t> lp;
+   gate<noise_window, int32_t> gt;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -214,15 +214,15 @@ struct controller
 ///////////////////////////////////////////////////////////////////////////////
 struct pitch_bend_controller
 {
-  void operator()(uint32_t val_)
-  {
-    uint32_t val = lp(val_);
-    if (gt(val))
-      midi_out << midi::pitch_bend{0, uint16_t{val << 4}};
-  }
+   void operator()(uint32_t val_)
+   {
+      uint32_t val = lp(val_);
+      if (gt(val))
+         midi_out << midi::pitch_bend{0, uint16_t{(val << 4) + (val % 16)}};
+   }
 
-  lowpass<64, int32_t> lp;
-  gate<noise_window, int32_t> gt;
+   lowpass<64, int32_t> lp;
+   gate<noise_window, int32_t> gt;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -230,97 +230,97 @@ struct pitch_bend_controller
 ///////////////////////////////////////////////////////////////////////////////
 struct program_change_controller
 {
-  program_change_controller()
-    : curr{0}
-    , base{0}
-  {}
+   program_change_controller()
+      : curr{0}
+      , base{0}
+   {}
 
-  void load()
-  {
-    if (!flash_b.empty())
-      base = flash_b.read();
-  }
+   void load()
+   {
+      if (!flash_b.empty())
+         base = flash_b.read();
+   }
 
-  void save()
-  {
-    uint8_t base_ = max(min(base, 127), 0);
-    if (base_ != flash_b.read())
-      flash_b.write(base_);
-  }
+   void save()
+   {
+      uint8_t base_ = max(min(base, 127), 0);
+      if (base_ != flash_b.read())
+         flash_b.write(base_);
+   }
 
-  uint8_t get()
-  {
-    return uint8_t{max(min(curr + base, 127), 0)};
-  }
+   uint8_t get()
+   {
+      return uint8_t{max(min(curr + base, 127), 0)};
+   }
 
-  void transmit()
-  {
-    midi_out << midi::program_change{0, get()};
-  }
+   void transmit()
+   {
+      midi_out << midi::program_change{0, get()};
+   }
 
-  void operator()(uint32_t val_)
-  {
-    uint32_t curr_ = curr * 205;
-    int diff = curr_ - val_;
-    if (diff < 0)
-      diff = -diff;
-    if (diff < 8)
-      return;
+   void operator()(uint32_t val_)
+   {
+      uint32_t curr_ = curr * 205;
+      int diff = curr_ - val_;
+      if (diff < 0)
+         diff = -diff;
+      if (diff < 8)
+         return;
 
-    uint8_t val = (val_ * 5) / 1024;
-    if (val != curr)
-    {
-      curr = val;
-      transmit();
-    }
-  }
+      uint8_t val = (val_ * 5) / 1024;
+      if (val != curr)
+      {
+         curr = val;
+         transmit();
+      }
+   }
 
-  void up(bool sw)
-  {
-    if (btn_up(sw) && (base < 127))
-    {
-      ++base;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void up(bool sw)
+   {
+      if (btn_up(sw) && (base < 127))
+      {
+         ++base;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  void down(bool sw)
-  {
-    if (btn_down(sw) && (base > 0))
-    {
-      --base;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void down(bool sw)
+   {
+      if (btn_down(sw) && (base > 0))
+      {
+         --base;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  void group_up(bool sw)
-  {
-    if (grp_btn_up(sw) && (base < 127))
-    {
-      base += 5;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void group_up(bool sw)
+   {
+      if (grp_btn_up(sw) && (base < 127))
+      {
+         base += 5;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  void group_down(bool sw)
-  {
-    if (grp_btn_down(sw) && (base > 0))
-    {
-      base -= 5;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void group_down(bool sw)
+   {
+      if (grp_btn_down(sw) && (base > 0))
+      {
+         base -= 5;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  int16_t curr;
-  int16_t base;
-  repeat_button<> btn_up;
-  repeat_button<> btn_down;
-  repeat_button<> grp_btn_up;
-  repeat_button<> grp_btn_down;
+   int16_t curr;
+   int16_t base;
+   repeat_button<> btn_up;
+   repeat_button<> btn_down;
+   repeat_button<> grp_btn_up;
+   repeat_button<> grp_btn_down;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -328,16 +328,16 @@ struct program_change_controller
 ///////////////////////////////////////////////////////////////////////////////
 struct sustain_controller
 {
-  void operator()(bool sw)
-  {
-    int state = edge(sw);
-    if (state == 1)
-      midi_out << midi::control_change{0, midi::cc::sustain, 0};
-    else if (state == -1)
-      midi_out << midi::control_change{0, midi::cc::sustain, 127};
-  }
+   void operator()(bool sw)
+   {
+      int state = edge(sw);
+      if (state == 1)
+         midi_out << midi::control_change{0, midi::cc::sustain, 0};
+      else if (state == -1)
+         midi_out << midi::control_change{0, midi::cc::sustain, 127};
+   }
 
-  edge_detector<> edge;
+   edge_detector<> edge;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -345,51 +345,51 @@ struct sustain_controller
 ///////////////////////////////////////////////////////////////////////////////
 struct bank_select_controller
 {
-  bank_select_controller()
-    : curr{0}
-  {}
+   bank_select_controller()
+      : curr{0}
+   {}
 
-  void load()
-  {
-    if (!flash_c.empty())
-      curr = flash_c.read();
-  }
+   void load()
+   {
+      if (!flash_c.empty())
+         curr = flash_c.read();
+   }
 
-  void save()
-  {
-    uint8_t curr_ = max(min(curr, 127), 0);
-    if (curr_ != flash_c.read())
-      flash_c.write(curr_);
-  }
+   void save()
+   {
+      uint8_t curr_ = max(min(curr, 127), 0);
+      if (curr_ != flash_c.read())
+         flash_c.write(curr_);
+   }
 
-  void transmit()
-  {
-    midi_out << midi::control_change{0, midi::cc::bank_select, curr};
-  }
+   void transmit()
+   {
+      midi_out << midi::control_change{0, midi::cc::bank_select, curr};
+   }
 
-  void up(bool sw)
-  {
-    if (btn_up(sw) && (curr < 127))
-    {
-      ++curr;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void up(bool sw)
+   {
+      if (btn_up(sw) && (curr < 127))
+      {
+         ++curr;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  void down(bool sw)
-  {
-    if (btn_down(sw) && (curr > 0))
-    {
-      --curr;
-      reset_save_delay();
-      transmit();
-    }
-  }
+   void down(bool sw)
+   {
+      if (btn_down(sw) && (curr > 0))
+      {
+         --curr;
+         reset_save_delay();
+         transmit();
+      }
+   }
 
-  uint8_t curr;
-  repeat_button<> btn_up;
-  repeat_button<> btn_down;
+   uint8_t curr;
+   repeat_button<> btn_up;
+   repeat_button<> btn_down;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -409,43 +409,43 @@ bank_select_controller                 bank_select_control;
 ///////////////////////////////////////////////////////////////////////////////
 void setup()
 {
-  pinMode(ch9 , INPUT_PULLUP);
-  pinMode(ch10, INPUT);
-  pinMode(ch11, INPUT);
-  pinMode(ch12, INPUT);
-  pinMode(ch13, INPUT);
-  pinMode(ch14, INPUT);
-  pinMode(ch15, INPUT);
+   pinMode(ch9 , INPUT_PULLUP);
+   pinMode(ch10, INPUT);
+   pinMode(ch11, INPUT);
+   pinMode(ch12, INPUT);
+   pinMode(ch13, INPUT);
+   pinMode(ch14, INPUT);
+   pinMode(ch15, INPUT);
 
-  pinMode(aux1, INPUT_PULLUP);
-  pinMode(aux2, INPUT_PULLUP);
-  pinMode(aux3, INPUT_PULLUP);
-  pinMode(aux4, INPUT_PULLUP);
-  pinMode(aux5, INPUT_PULLUP);
-  pinMode(aux6, INPUT_PULLUP);
+   pinMode(aux1, INPUT_PULLUP);
+   pinMode(aux2, INPUT_PULLUP);
+   pinMode(aux3, INPUT_PULLUP);
+   pinMode(aux4, INPUT_PULLUP);
+   pinMode(aux5, INPUT_PULLUP);
+   pinMode(aux6, INPUT_PULLUP);
 
-  midi_out.start();
+   midi_out.start();
 
-  // Load the program_change and bank_select_control states from flash
-  program_change.load();
-  bank_select_control.load();
+   // Load the program_change and bank_select_control states from flash
+   program_change.load();
+   bank_select_control.load();
 
-  // Transmit the current program_change and bank select state
-  program_change.transmit();
-  bank_select_control.transmit();
+   // Transmit the current program_change and bank select state
+   program_change.transmit();
+   bank_select_control.transmit();
 
 #ifdef NEXUS_DUMP_FLASH
-  unsigned char* seg_b = SEGMENT_B;
-  midi_out << midi::sysex<16> {0x5555, seg_b};
-  midi_out << midi::sysex<16> {0x5555, seg_b + 16};
-  midi_out << midi::sysex<16> {0x5555, seg_b + 32};
-  midi_out << midi::sysex<16> {0x5555, seg_b + 48};
+   unsigned char* seg_b = SEGMENT_B;
+   midi_out << midi::sysex<16> {0x5555, seg_b};
+   midi_out << midi::sysex<16> {0x5555, seg_b + 16};
+   midi_out << midi::sysex<16> {0x5555, seg_b + 32};
+   midi_out << midi::sysex<16> {0x5555, seg_b + 48};
 
-  unsigned char* seg_c = SEGMENT_C;
-  midi_out << midi::sysex<16> {0x5555, seg_c};
-  midi_out << midi::sysex<16> {0x5555, seg_c + 16};
-  midi_out << midi::sysex<16> {0x5555, seg_c + 32};
-  midi_out << midi::sysex<16> {0x5555, seg_c + 48};
+   unsigned char* seg_c = SEGMENT_C;
+   midi_out << midi::sysex<16> {0x5555, seg_c};
+   midi_out << midi::sysex<16> {0x5555, seg_c + 16};
+   midi_out << midi::sysex<16> {0x5555, seg_c + 32};
+   midi_out << midi::sysex<16> {0x5555, seg_c + 48};
 #endif
 }
 
@@ -457,101 +457,101 @@ void setup()
 void loop()
 {
 #ifdef NEXUS_TEST_NOTE
-  _note(digitalRead(aux1));
+   _note(digitalRead(aux1));
 #endif
 
 #ifdef NEXUS_TEST_VOLUME
-  volume_control(analog_read(ch10));
+   volume_control(analog_read(ch10));
 #endif
 
 #ifdef NEXUS_TEST_PITCH_BEND
-  pitch_bend(analog_read(ch10));
+   pitch_bend(analog_read(ch10));
 #endif
 
 #ifdef NEXUS_TEST_PROGRAM_CHANGE
-  program_change(analog_read(ch15));
+   program_change(analog_read(ch15));
 #endif
 
 #ifdef NEXUS_TEST_PROGRAM_CHANGE_UP_DOWN
-  program_change.up(digitalRead(ch12));
-  program_change.down(digitalRead(ch13));
+   program_change.up(digitalRead(ch12));
+   program_change.down(digitalRead(ch13));
 #endif
 
 #ifdef NEXUS_TEST_PROGRAM_CHANGE_GROUP_UP_DOWN
-  program_change.group_up(digitalRead(ch12));
-  program_change.group_down(digitalRead(ch13));
+   program_change.group_up(digitalRead(ch12));
+   program_change.group_down(digitalRead(ch13));
 #endif
 
 #ifdef NEXUS_TEST_EFFECTS_1
-  fx1_control(analog_read(ch11));
+   fx1_control(analog_read(ch11));
 #endif
 
 #ifdef NEXUS_TEST_EFFECTS_2
-  fx2_control(analog_read(ch11));
+   fx2_control(analog_read(ch11));
 #endif
 
 #ifdef NEXUS_TEST_MODULATION
-  modulation_control(analog_read(ch11));
+   modulation_control(analog_read(ch11));
 #endif
 
 #ifdef NEXUS_TEST_SUSTAIN
-  sustain_control(digitalRead(ch12));
+   sustain_control(digitalRead(ch12));
 #endif
 
 #ifdef NEXUS_TEST_BANK_SELECT
-  bank_select_control.up(digitalRead(aux1));
-  bank_select_control.down(digitalRead(aux2));
+   bank_select_control.up(digitalRead(aux1));
+   bank_select_control.down(digitalRead(aux2));
 #endif
 
-  // Save the program_change and bank_select_control if needed
-  if ((save_delay_start_time != -1)
-      && (millis() > (save_delay_start_time + save_delay)))
-  {
-    program_change.save();
-    bank_select_control.save();
-    save_delay_start_time = -1;
-  }
+   // Save the program_change and bank_select_control if needed
+   if ((save_delay_start_time != -1)
+         && (millis() > (save_delay_start_time + save_delay)))
+   {
+      program_change.save();
+      bank_select_control.save();
+      save_delay_start_time = -1;
+   }
 }
 
 #else // !NEXUS_TEST
 
 uint16_t analog_read(uint16_t pin)
 {
-  uint16_t const min_x = 5;
-  uint16_t const max_x = 890;
-  uint16_t x = analogRead(pin);
-  if (x < min_x)
-    x = min_x;
-  else if (x > max_x)
-    x = max_x;
-  return map(x, min_x, max_x, 0, 1023);
+   uint16_t const min_x = 5;
+   uint16_t const max_x = 1023;
+   uint16_t x = analogRead(pin);
+   if (x < min_x)
+      x = min_x;
+   else if (x > max_x)
+      x = max_x;
+   return map(x, min_x, max_x, 0, 1023);
 }
 
 void loop()
 {
-  sustain_control(digitalRead(ch9));
-  volume_control(analog_read(ch10));
-  fx1_control(analog_read(ch11));
-  fx2_control(analog_read(ch12));
+   sustain_control(digitalRead(ch9));
+   volume_control(analog_read(ch10));
+   fx1_control(analog_read(ch11));
+   fx2_control(analog_read(ch12));
    pitch_bend(analog_read(ch13));
-  program_change(analog_read(ch14));
-  modulation_control(analog_read(ch15));
+   program_change(analog_read(ch14));
+   modulation_control(analog_read(ch15));
 
-  program_change.up(!digitalRead(aux1));
-  program_change.down(!digitalRead(aux2));
-  program_change.group_up(!digitalRead(aux3));
-  program_change.group_down(!digitalRead(aux4));
-  bank_select_control.up(!digitalRead(aux5));
-  bank_select_control.down(!digitalRead(aux6));
+   program_change.up(!digitalRead(aux1));
+   program_change.down(!digitalRead(aux2));
+   program_change.group_up(!digitalRead(aux3));
+   program_change.group_down(!digitalRead(aux4));
+   bank_select_control.up(!digitalRead(aux5));
+   bank_select_control.down(!digitalRead(aux6));
 
-  // Save the program_change and bank_select_control if needed
-  if ((save_delay_start_time != -1)
-      && (millis() > (save_delay_start_time + save_delay)))
-  {
-    program_change.save();
-    bank_select_control.save();
-    save_delay_start_time = -1;
-  }
+   // Save the program_change and bank_select_control if needed
+   if ((save_delay_start_time != -1)
+         && (millis() > (save_delay_start_time + save_delay)))
+   {
+      program_change.save();
+      bank_select_control.save();
+      save_delay_start_time = -1;
+   }
 }
 
 #endif // NEXUS_TEST
