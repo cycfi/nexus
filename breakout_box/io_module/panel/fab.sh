6d22c7f2c707587b20c4a3bb8421766b0087dd89
djowel <djowel@gmail.com>
2023-03-06 20:14:02 +0800

    PCB panel utilizing vcuts

--- breakout_box/io_module/panel/fab.sh
+++ breakout_box/io_module/panel/fab.sh
@@ -1,13 +1,12 @@
 kikit panelize \
-    --layout 'grid; rows: 2; cols: 2; space: 2mm' \
-    --tabs 'fixed; vwidth: 6mm; vcount: 2' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.251mm; prolong: -0.3mm' \
-    --source 'tolerance: 10mm' \
+    --layout 'grid; rows: 3; cols: 2; space: 0mm' \
+    --tabs full \
+    --cuts vcuts \
     --post 'millradius: 1mm' \
-    --framing 'railstb; width: 5mm; space: 3mm;' \
+    --framing 'railstb; width: 3mm; space: 3mm;' \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
     --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
-    --text 'simple; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
     --post 'millradius: 1mm' \
     ../io_module.kicad_pcb io_module_panel.kicad_pcb
 
