07c22eb0717f82150444cc804a5ce0664df91450
djowel <djowel@gmail.com>
2025-05-08 13:22:15 +0800

    Production run May 2025

--- breakout_box/main_board/panel/fab.sh
+++ breakout_box/main_board/panel/fab.sh
@@ -1,9 +1,9 @@
 kikit panelize \
-    --layout 'grid; rows: 1; cols: 2; space: 0mm' \
-    --tabs full \
-    --cuts vcuts \
+    --layout 'grid; rows: 1; cols: 2; space: 3mm' \
+    --tabs 'fixed; width: 10mm; vcount: 2; hcount: 4' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.3mm; prolong: -0.3mm' \
     --post 'millradius: 1mm' \
-    --framing 'railstb; width: 3mm; space: 3mm;' \
+    --framing 'frame; width: 5mm; space: 3mm;' \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
     --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
     --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
