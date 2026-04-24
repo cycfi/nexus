80c06952a9c2082d1d0e45ac19c01faa7f4f7ccb
djowel <djowel@gmail.com>
2026-04-24 20:37:51 +0800

    Remove redundant 'post'

--- breakout_box/io_module/panel/fab.sh
+++ breakout_box/io_module/panel/fab.sh
@@ -7,7 +7,6 @@ kikit panelize \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
     --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
     --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
-    --post 'millradius: 1mm' \
     ../io_module.kicad_pcb io_module_panel.kicad_pcb
 
 kikit fab jlcpcb --no-drc io_module_panel.kicad_pcb .
